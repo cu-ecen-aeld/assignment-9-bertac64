e7428ac3a9bfd88abb4b74cae720f7e07ec2a6e9
bertac64 <c.bertacchini@igeamedical.com>
2024-06-26 10:10:01 +0200

    File Fix

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -20,11 +20,11 @@ endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d 0755 $(@D)/finder-app/writer.c $(TARGET_DIR)/root/
-	$(INSTALL) -d 0755 $(@D)/finder-app/Makefile $(TARGET_DIR)/root/
-	$(INSTALL) -d 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/root/
-	$(INSTALL) -d 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/root/
-	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -D 0755 $(@D)/finder-app/writer.c $(TARGET_DIR)/root/
+	$(INSTALL) -D 0755 $(@D)/finder-app/Makefile $(TARGET_DIR)/root/
+	$(INSTALL) -D 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/root/
+	$(INSTALL) -D 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/root/
+	$(INSTALL) -D 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
 endef
