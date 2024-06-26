77b70d934930c4f1a78451a3cdf97119e7aea680
bertac64 <c.bertacchini@igeamedical.com>
2024-06-26 10:04:13 +0200

    File update

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -20,9 +20,10 @@ endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d 0755 $(@D)/finder-app/ $(TARGET_DIR)/etc/finder-app/writer/
-	$(INSTALL) -d 0755 $(@D)/finder-app/ $(TARGET_DIR)/etc/finder-app/finder.sh/
-	$(INSTALL) -d 0755 $(@D)/finder-app/ $(TARGET_DIR)/etc/finder-app/finder-test.sh/
+	$(INSTALL) -d 0755 $(@D)/finder-app/writer.c $(TARGET_DIR)/root/
+	$(INSTALL) -d 0755 $(@D)/finder-app/Makefile $(TARGET_DIR)/root/
+	$(INSTALL) -d 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/root/
+	$(INSTALL) -d 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/root/
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
