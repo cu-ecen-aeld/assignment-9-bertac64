ba2c15ad20ceae12bf537b33877aba231eee9356
bertac64 <c.bertacchini@igeamedical.com>
2024-06-28 09:30:59 +0200

    Update aesd-assignments.mk

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = v1.2.0
+AESD_ASSIGNMENTS_VERSION = assignments-3-and-later-bertac64
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
