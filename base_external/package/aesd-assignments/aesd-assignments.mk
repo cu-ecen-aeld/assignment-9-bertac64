5f276552150a967c716d009b03ca7a7c4993834c
bertac64 <c.bertacchini@igeamedical.com>
2024-10-07 16:02:50 +0200

    assignment-9 complete
    Updated file aesd-assignments.mk with reference to the last assignment-3and-later software version

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = v1.9.0
+AESD_ASSIGNMENTS_VERSION = 5334a792ea43f111f23f9e975fec9716051a16f8
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
